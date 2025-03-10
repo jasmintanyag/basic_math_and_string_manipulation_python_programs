add204ad35aac6d744bce523009c22a7f2b666b4
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:40:30 +0100

    Added pseudocode

---
+++ quotient_with_decimal.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# ask user to input 2 numbers
+# check if the second number is 0
+# if zero, print "Cannot divide by zero."
+# print the quotient of two numbers
