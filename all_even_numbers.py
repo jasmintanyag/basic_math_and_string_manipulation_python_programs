eb96d2d969c58d950b694b179d01cff321507e1b
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 00:10:39 +0100

    Rearrange

--- all_even_numbers.py
+++ all_even_numbers.py
@@ -1,8 +1,11 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
-# create a for loop, from numbers 0 to 100
+'''
+create a for loop, from numbers 0 to 100
+check if even number
+print all even numbers
+'''
+
 for num in range(101):
-# check if even number
-# print all even numbers
     if num % 2 == 0:
         print(num, end = " ")
