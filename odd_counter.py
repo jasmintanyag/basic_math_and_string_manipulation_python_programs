14a1b2a5041d980029468ae44bd86321130c7c88
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 23:41:45 +0100

    Initialize and Loop

--- odd_counter.py
+++ odd_counter.py
@@ -1,7 +1,9 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 # initialize odd count to 0 before the loop
+odd_count = 0
 # loop from 1 to 10
+for i in range(10):
 # ask the user to enter a number 10 times (since the range is 10)
 # convert the input to a float
 # check if the number is not divisible by 2
