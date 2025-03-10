b651540ed0aeef405a0379a9b04fc3871913d722
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:10:55 +0100

    Made the presentation much better

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -1,7 +1,10 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+print the sum of two numbers'
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# print the sum of two numbers
-print(num1 + num2)
+print(f"The sum of the two number is: {num1 + num2}")
