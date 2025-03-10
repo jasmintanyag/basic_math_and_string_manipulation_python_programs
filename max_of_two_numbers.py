584cf145e96759e851a131a271deef9581d2c604
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 21:13:53 +0100

    added pseudocode

---
+++ max_of_two_numbers.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# ask user to input 2 numbers.
+# print the bigger number by using built-in function max().
+
