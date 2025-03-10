adfb97847fe40ba803bd742375dea5ed3646d590
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 23:02:49 +0100

    Rearrange

--- first_raise_to_second.py
+++ first_raise_to_second.py
@@ -1,7 +1,10 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+print the result of first num raised to second num
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# print the result of first num raised to second num
-print(num1 ** num2)
+print(f"The result of {num1} raised to {num2} is: {num1 ** num2}")
