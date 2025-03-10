236fe077170640343c2ccff0406b7a8ed4d2c02d
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 21:41:50 +0100

    Added pseudocode

---
+++ print_equal_if_same_number.py
@@ -0,0 +1,5 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# ask user to input 2 numbers
+# check if the 2 numbers are equal
+# print "Equal" if the 2 numbers are the same
