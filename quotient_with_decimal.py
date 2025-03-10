c224f374db9348334da7e04e7c6c1d99a54c72ed
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:44:56 +0100

    Cannot divide by zero.

--- quotient_with_decimal.py
+++ quotient_with_decimal.py
@@ -5,4 +5,6 @@ num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
 # check if the second number is 0
 # if zero, print "Cannot divide by zero."
+if num2 == 0:
+    print("Cannot divide by zero.")
 # print the quotient of two numbers
