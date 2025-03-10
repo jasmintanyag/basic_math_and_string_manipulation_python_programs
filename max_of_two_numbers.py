09b6f5facdabff2aeb57c6537ecbbaa17243c68b
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 21:20:46 +0100

    ask user to input 2 numbers

--- max_of_two_numbers.py
+++ max_of_two_numbers.py
@@ -1,5 +1,7 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # ask user to input 2 numbers.
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # print the bigger number by using built-in function max().
 
