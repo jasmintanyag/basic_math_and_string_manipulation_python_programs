4348890c669947b5294a20fe553ddd89a587030d
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:50:59 +0100

    Rearrange

--- quotient_with_decimal.py
+++ quotient_with_decimal.py
@@ -1,12 +1,15 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
+'''
 # ask user to input 2 numbers
-num1 = float(input("Enter a number: "))
-num2 = float(input("Enter another number: "))
 # check if the second number is 0
 # if zero, print "Cannot divide by zero."
+# print the quotient of two numbers
+'''
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 if num2 == 0:
     print("Cannot divide by zero.")
-# print the quotient of two numbers
 else:
-    print(num1 / num2)
+    print(f"The quotient of the two number is: {num1 / num2}")
