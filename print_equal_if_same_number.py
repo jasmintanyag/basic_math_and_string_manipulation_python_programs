047cd038acfb24bb1e4c046c52ab64b9aa94d082
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 21:46:42 +0100

    Use if statement to check if the 2 numbers are equal

--- print_equal_if_same_number.py
+++ print_equal_if_same_number.py
@@ -3,5 +3,6 @@
 # ask user to input 2 numbers
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# check if the 2 numbers are equal
+# use if statement to check if the 2 numbers are equal
+if num1 == num2:
 # print "Equal" if the 2 numbers are the same
