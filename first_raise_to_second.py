6e632d08bbd1993f152c261c96704b43e57b4b2a
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:58:28 +0100

    Ask user to input 2 numbers

--- first_raise_to_second.py
+++ first_raise_to_second.py
@@ -1,4 +1,6 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # print the result of first num raised to second num
