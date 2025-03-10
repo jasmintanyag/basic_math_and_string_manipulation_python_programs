6712ba4b95bd88fc5b9588cce4f0e6903d667219
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:55:59 +0100

    Added pseudocode

---
+++ first_raise_to_second.py
@@ -0,0 +1,4 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+# ask user to input 2 numbers
+# print the result of first num raised to second num
