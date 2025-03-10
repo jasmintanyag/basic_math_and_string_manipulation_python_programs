9eb911a1b55f6c2c98579774d42aa72850409baa
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 23:31:25 +0100

    Rearrange

--- sum_of_ten_numbers.py
+++ sum_of_ten_numbers.py
@@ -1,13 +1,16 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
-# initialize total number of sum to 0 before the loop
+'''
+initialize total number of sum to 0 before the loop
+loop from 1 to 10
+ask the user to enter a number 10 times (since the range is 10)
+convert the input to a float
+add the number to total
+print the total sum
+'''
+
 total_sum = 0
-# loop from 1 to 10
-# ask the user to enter a number 10 times (since the range is 10)
-# convert the input to a float
 for i in range(10):
     num = float(input(f"Enter a number ({i+1}):  "))
-# add the number to total
     total_sum += num
-# print the total sum
-print(total_sum)
+print(f"The sum of all the numbers are: {total_sum}")
