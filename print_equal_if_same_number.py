1bb2352aa790003c5c31f874fe43b34b4d6304ce
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 21:52:29 +0100

    Rearrange for better presentation

--- print_equal_if_same_number.py
+++ print_equal_if_same_number.py
@@ -1,9 +1,12 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+use if statement to check if the 2 numbers are equal
+print "Equal" if the 2 numbers are the same'
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# use if statement to check if the 2 numbers are equal
 if num1 == num2:
-# print "Equal" if the 2 numbers are the same
-    print("Equal")
+    print("The two numbers are Equal.")
