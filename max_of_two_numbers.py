55798ef831086591439b92d3a0f11c60406da7e6
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 21:32:28 +0100

    added some words for better presentation

--- max_of_two_numbers.py
+++ max_of_two_numbers.py
@@ -1,7 +1,8 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-
-# ask user to input 2 numbers.
+'''
+ask user to input 2 numbers.
+print the bigger number by using built-in function max().
+'''
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# print the bigger number by using built-in function max().
-print(max(num1, num2))
+print(f"The bigger number is: {max(num1, num2)}")
