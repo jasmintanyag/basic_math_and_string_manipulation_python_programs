56a63763f7d6b0a7d5bbdd6cd02b4d30d3916d2b
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:28:25 +0100

    Ask user to input 2 numbers

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,4 +1,6 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # print the product of two numbers
