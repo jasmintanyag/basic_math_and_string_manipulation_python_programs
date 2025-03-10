313dc3b911a86a4bf26e6f1c836b7bcb6e3f92b9
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 23:40:01 +0100

    Added pseudocode

---
+++ odd_counter.py
@@ -0,0 +1,9 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# initialize odd count to 0 before the loop
+# loop from 1 to 10
+# ask the user to enter a number 10 times (since the range is 10)
+# convert the input to a float
+# check if the number is not divisible by 2
+# if it is not divisible, add 1 to odd count 
+# print the odd count
