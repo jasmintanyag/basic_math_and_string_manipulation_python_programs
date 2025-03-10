9b88d6df18d0a63df329053ca25348110ff3b364
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 00:05:47 +0100

    fixed error

--- all_even_numbers.py
+++ all_even_numbers.py
@@ -1,7 +1,7 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
 # create a for loop, from numbers 0 to 100
-for num in range(100):
+for num in range(101):
 # check if even number
 # print all even numbers
     if num % 2 == 0:
