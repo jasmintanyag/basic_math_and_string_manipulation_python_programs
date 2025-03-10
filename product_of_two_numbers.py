f7b6a63a3d9b48e2cf153e9bade528d42aa97175
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-10 22:35:50 +0100

    Made the program look more presentable

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,7 +1,10 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+print the product of two numbers
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# print the product of two numbers'
-print(num1 * num2)
+print(f"The product of the two number is: {num1 * num2}")
