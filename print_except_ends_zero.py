cc65629e4c33af39baa79bc640bdc2aa91e33225
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 00:15:09 +0100

    Added pseudocode

---
+++ print_except_ends_zero.py
@@ -0,0 +1,5 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# create a for loop, from numbers 0 to 100
+# check if the number is not divisible by 10
+# if not, print the numbers
