0ad050bc046ee2c0b143a5b5f3d6c4074773628f
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 00:21:55 +0100

    Rearrange

--- print_except_ends_zero.py
+++ print_except_ends_zero.py
@@ -1,8 +1,11 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
-# create a for loop, from numbers 0 to 100
+'''
+create a for loop, from numbers 0 to 100
+check if the number is not divisible by 10
+if not, print the numbers
+'''
+
 for num in range(101):
-# check if the number is not divisible by 10
-# if not, print the numbers
     if num % 10 != 0:
         print(num, end= " ")
