6f06ef6ca2bcfb67ac68369e6e7b6968d5304471
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:25:43 +0100

    cannon divide bt zeo

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -5,5 +5,7 @@ num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
 # check if the second number is 0
 # if zero, print "Cannot divide by zero."
+if num2 == 0:
+    print("Cannot divide by zero.")
 # else, perform a floor division
 # convert the result into integer
