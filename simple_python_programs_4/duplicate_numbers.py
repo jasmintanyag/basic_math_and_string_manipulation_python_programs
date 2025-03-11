01a1d4fc2093aea3b735077500cad0af81182a83
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 21:54:46 +0100

    Convert to integer

--- simple_python_programs_4/duplicate_numbers.py
+++ simple_python_programs_4/duplicate_numbers.py
@@ -5,9 +5,9 @@ num_list = []       # list to store user input
 duplicate_nums = [] # list to store duplicate numbers
 num_counts = {}     # dictionary to store number counts
 # ask user to input 10 numbers, using for loop
-for i in range(10):
-    entered_num = input(f"Enter number ({i+1}): ")
 # convert into integer
+for i in range(10):
+    entered_num = int(input(f"Enter number ({i+1}): "))
 # add the entered number into the list
 # count how many times the number has been entered
 # add 1 if the number already exists
