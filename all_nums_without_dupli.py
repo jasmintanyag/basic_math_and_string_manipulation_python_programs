c695d038ef56a47804fceac8be2595cce104600f
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 19:57:49 +0100

    Added pseudocode

---
+++ all_nums_without_dupli.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# initialize variable to store user input
+# ask user to input 10 numbers using for loop
+# convert into integer
+# check if num is in the list, if not, add it
+# print the numbers
