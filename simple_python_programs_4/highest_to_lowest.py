c9a1acf3aeb51af3dac2b13da12ea6b8a0ffebe6
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 23:12:49 +0100

    Added pseudocoed

---
+++ simple_python_programs_4/highest_to_lowest.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+# initialize variable to store user input, (empty list)
+# create infinite loop to continuously ask user to input numbers
+# add the entered number to the list
+# if user entered an invalid input, stop the program
+# sort the entered numbers from highest to  lowest using sort(reverse) function
+# display the numbers from highest to  lowest
