c32a00da55a0a1295701894555e76d7edfa79220
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 02:40:54 +0100

    Ask user to input 2 numbers

--- min_of_two_numbers.py
+++ min_of_two_numbers.py
@@ -1,4 +1,6 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # print the smaller number by using built-in function min()
