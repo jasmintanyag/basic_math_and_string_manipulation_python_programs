d35f0ecfbd367e7bdc9052de86072bb1023152a5
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:14:29 +0100

    fixed typo

--- simple_python_programs_1/quotient_with_decimal.py
+++ simple_python_programs_1/quotient_with_decimal.py
@@ -1,10 +1,10 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 '''
-# ask user to input 2 numbers
-# check if the second number is 0
-# if zero, print "Cannot divide by zero."
-# print the quotient of two numbers
+ask user to input 2 numbers
+check if the second number is 0
+if zero, print "Cannot divide by zero."
+print the quotient of two numbers
 '''
 
 num1 = float(input("Enter a number: "))
