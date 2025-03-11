00c978377f06975609059279f74afec4c1ace15f
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:17:57 +0100

    Loop with range(101)

--- except_ends_zero_five.py
+++ except_ends_zero_five.py
@@ -1,5 +1,6 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # create a for loop, from numbers 0 to 100
+for num in range(101):
 # check if the number is not divisible by 5
 # if not, print the numbers
