b9927f51dd9679ac01dfe72a99601914f6953bd2
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 13:23:27 +0100

    Convert to float

--- num_minus_remaining_numbers.py
+++ num_minus_remaining_numbers.py
@@ -5,8 +5,8 @@ total_diff = 0
 # loop from 1 to 10
 for i in range(10):
 # ask the user to enter a number 10 times (since the range is 10)
-    num = input(f"Enter a number ({i+1}):  ")
 # convert the input to a float
+    num = float(input(f"Enter a number ({i+1}):  "))
 # if it's the first number, add it to total difference
 # else, subract the number from total difference
 # print the total difference
