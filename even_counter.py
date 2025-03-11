507b77e1d54bfd53db90cb626cc9f5b2e46e591c
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 13:47:32 +0100

    Rearrange

--- even_counter.py
+++ even_counter.py
@@ -1,15 +1,18 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
-# initialize even count to 0 before the loop
+'''
+initialize even count to 0 before the loop
+loop from 1 to 10
+ask the user to enter a number 10 times (since the range is 10)
+convert the input to a float
+check if the number is divisible by 2
+if it is divisible, add 1 to odd count
+print the even count
+'''
+
 even_count = 0
-# loop from 1 to 10
 for i in range(10):
-# ask the user to enter a number 10 times (since the range is 10)
-# convert the input to a float
     num = float(input(f"Enter a number ({i+1}): "))
-# check if the number is divisible by 2
     if num % 2 == 0:
-# if it is divisible, add 1 to odd count
         even_count += 1
-# print the even count
-print(even_count)
+print(f"The number of even numbers are: {even_count}")
