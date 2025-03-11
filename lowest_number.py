ec93fa8a8dc256045df77de98d2049991519aa59
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 20:53:52 +0100

    Used infinite loop, asked user to input number

--- lowest_number.py
+++ lowest_number.py
@@ -1,7 +1,11 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 # initialize variable to store user input, (empty list)
+list_nums = []
 # create infinite loop to continuously ask user to input numbers
+while True:
+    try:
+        num = float(input("Enter a number: "))
 # add the entered number to the list
 # if user entered an invalid input, stop the program
 # print the lowest number that has been entered
