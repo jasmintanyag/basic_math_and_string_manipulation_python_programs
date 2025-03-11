8fd34bb2eaed56b7a4906c99677db3009396d9cf
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 02:57:21 +0100

    Rearrange

--- not_equal_not_same_number.py
+++ not_equal_not_same_number.py
@@ -1,9 +1,12 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+use if statement to check if the 2 numbers are not equal
+print "Not Equal" if the 2 numbers are not the same
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# use if statement to check if the 2 numbers are not equal
 if num1 != num2:
-# print "Not Equal" if the 2 numbers are not the same
     print("Not Equal")
