0bb17dc443c72340334422379772ceb0f2221669
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 21:50:57 +0100

    Added pseudocode

---
+++ simple_python_programs_4/duplicate_numbers.py
@@ -0,0 +1,15 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# initialize the variables for storing the numbers
+    # list to store user input
+    # list to store duplicate  numbers
+    # dictionary to store number counts
+# ask user to input 10 numbers, using for loop
+# convert into integer
+# add the entered number into the list
+# count how many times the number has been entered
+# add 1 if the number already exists
+# initialize count to 1 if it's a new number
+# check the numbers that have duplicates
+    # if the number appears more than once, add it to duplicate list
+# print the number/s that have duplicate/s
