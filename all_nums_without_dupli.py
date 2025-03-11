b1e175c8aa73bdfaa3f9626424125ca98a77eb1c
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 19:58:26 +0100

    Initialize

--- all_nums_without_dupli.py
+++ all_nums_without_dupli.py
@@ -1,6 +1,7 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 # initialize variable to store user input
+entered_num = []
 # ask user to input 10 numbers using for loop
 # convert into integer
 # check if num is in the list, if not, add it
