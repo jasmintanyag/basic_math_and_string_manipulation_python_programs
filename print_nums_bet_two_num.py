3434cb8498033c75e03de139914a5c5050cc05ff
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:30:17 +0100

    Added pseudocode

---
+++ print_nums_bet_two_num.py
@@ -0,0 +1,5 @@
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask user to input 2 numbers
+# create loop to get the numbers between 2 number
+# print the numbers in between
