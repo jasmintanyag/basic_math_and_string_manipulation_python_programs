fbbc0d6b555de5aeadafc03cf16dd5278c0f3f75
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:32:07 +0100

    Added step

--- print_nums_bet_two_num.py
+++ print_nums_bet_two_num.py
@@ -1,5 +1,6 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # ask user to input 2 numbers
+# convert into integer
 # create loop to get the numbers between 2 number
 # print the numbers in between
