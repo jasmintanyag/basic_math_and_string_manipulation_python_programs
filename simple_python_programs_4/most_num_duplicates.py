9551d982821818e8404a086303ed1b435e2f1398
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 22:32:58 +0100

    Added pseudocode

---
+++ simple_python_programs_4/most_num_duplicates.py
@@ -0,0 +1,11 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# initialize variable to store how often the number repeats
+# create infinite loop to continuously ask user to input numbers
+# count how often the number occur
+# add 1 to count if the number already exists
+# initialize count to 1 if it's the first appearance
+# stop the program when there's invalid input
+# identify the number that has most duplicates
+# print the most frequent num
+# else, print "there's no valid number entered"
