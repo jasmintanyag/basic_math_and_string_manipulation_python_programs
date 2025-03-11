94bf914c5d56433626fe3a1f6b20360aa4df145f
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 02:46:15 +0100

    added some words for better presentation

--- min_of_two_numbers.py
+++ min_of_two_numbers.py
@@ -1,7 +1,10 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+# print the smaller number by using built-in function min()
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# print the smaller number by using built-in function min()
-print(min(num1, num2))
+print(f"The smaller number is: {min(num1, num2)}")
