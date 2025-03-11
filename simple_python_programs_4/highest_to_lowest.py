0112b4b1c46101e13159dc03afd1bc7e289fb7b6
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 23:14:32 +0100

    create infinite loop to continuously ask user to input numbers

--- simple_python_programs_4/highest_to_lowest.py
+++ simple_python_programs_4/highest_to_lowest.py
@@ -3,6 +3,9 @@
 # initialize variable to store user input, (empty list)
 list_of_all_nums = []
 # create infinite loop to continuously ask user to input numbers
+while  True:
+    try:
+        num = float(input("Enter a number: "))
 # add the entered number to the list
 # if user entered an invalid input, stop the program
 # sort the entered numbers from highest to  lowest using sort(reverse) function
