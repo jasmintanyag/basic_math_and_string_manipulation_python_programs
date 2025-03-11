1af24f4eba62952a48bac9857d88edf74fe2bfbe
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:23:55 +0100

    Ask user to input 2 numbers

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -1,6 +1,8 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # check if the second number is 0
 # if zero, print "Cannot divide by zero."
 # else, perform a floor division
