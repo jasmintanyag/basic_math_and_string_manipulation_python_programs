684b901842cd7196ac328d1e849d59b6b7a9fc91
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 13:36:37 +0100

    Added pseudocode

---
+++ even_counter.py
@@ -0,0 +1,9 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# initialize even count to 0 before the loop
+# loop from 1 to 10
+# ask the user to enter a number 10 times (since the range is 10)
+# convert the input to a float
+# check if the number is divisible by 2
+# if it is divisible, add 1 to odd count 
+# print the odd count
