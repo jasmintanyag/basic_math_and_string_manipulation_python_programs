19779e6b743286e50124dd718e719c715a484955
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 02:51:44 +0100

    Ask user to input 2 numbers

--- not_equal_not_same_number.py
+++ not_equal_not_same_number.py
@@ -1,5 +1,7 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # use if statement to check if the 2 numbers are not equal
 # print "Not Equal" if the 2 numbers are not the same
