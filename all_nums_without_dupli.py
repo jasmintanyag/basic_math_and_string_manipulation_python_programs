2c1d19844b4116eb2bcab1acbc2c39e7d2343eb3
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 20:02:59 +0100

    Check if num is in the list, if not, add it

--- all_nums_without_dupli.py
+++ all_nums_without_dupli.py
@@ -7,4 +7,6 @@ entered_num = []
 for i in range(10):
     num = int(input(f"Enter number ({i+1}): "))
 # check if num is in the list, if not, add it
+    if num not in entered_num:
+        entered_num.append(num)
 # print the numbers
