5e4a24d8000de2e9b75490197761c2182bef77f2
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 13:14:22 +0100

    Added pseudocode

---
+++ num_minus_remaining_numbers.py
@@ -0,0 +1,9 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+# initialize total difference to 0 before the loop
+# loop from 1 to 10
+# ask the user to enter a number 10 times (since the range is 10)
+# convert the input to a float
+# if it's the first number, add it to total difference
+# else, subract the number from total difference
+# print the total difference
