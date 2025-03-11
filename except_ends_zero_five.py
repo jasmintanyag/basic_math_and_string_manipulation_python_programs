e52f527bf4e983124135ddb3825554848d6d5338
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:22:26 +0100

    Rearrange

--- except_ends_zero_five.py
+++ except_ends_zero_five.py
@@ -1,8 +1,11 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
-# create a for loop, from numbers 0 to 100
+'''
+create a for loop, from numbers 0 to 100
+check if the number is not divisible by 5
+if not, print the numbers
+'''
+
 for num in range(101):
-# check if the number is not divisible by 5
     if num % 5 != 0:
-# if not, print the numbers
         print(num, end= " ")
