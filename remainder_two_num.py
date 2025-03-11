abcc730420fee21d0c8aa4c64d20d1deb3130a73
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:43:52 +0100

    Ask user to input 2 numbers

--- remainder_two_num.py
+++ remainder_two_num.py
@@ -1,4 +1,6 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # print the remainder of first num and second num
