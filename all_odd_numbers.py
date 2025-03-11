f077e12f037a8b1e18d1c12777e19e0c225298f8
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:06:41 +0100

    Rearrange

--- all_odd_numbers.py
+++ all_odd_numbers.py
@@ -1,11 +1,14 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-# initialize num to 1
+'''
+initialize num to 1
+create a while loop, run while num is less than or equal to 100
+print num
+increase num by 2 to get the next odd numbers
+repeat until num exceeds 100
+'''
+
 num = 1
-# create a while loop, run while num is less than or equal to 100
 while num <= 100:
-# print num
     print(num, end= " ")
-# increase num by 2 to get the next odd numbers
-# repeat until num exceeds 100
     num += 2
