c4a53a481d6ae2adf814ff0da7bdd6e58e57fb5e
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:36:38 +0100

    loop to get numbers between 2 num

--- print_nums_bet_two_num.py
+++ print_nums_bet_two_num.py
@@ -4,5 +4,6 @@
 # convert into integer
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
-# create loop to get the numbers between 2 number
+# create loop to get the numbers between 1st num and 2nd num
+for numbers in range(num1 + 1, num2):
 # print the numbers in between
