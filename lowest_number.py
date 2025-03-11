71134e2a691910ac212c4129e0da86e05ab09ed9
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 20:52:09 +0100

    Added pseudocode

---
+++ lowest_number.py
@@ -0,0 +1,7 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# initialize variable to store user input, (empty list)
+# create infinite loop to continuously ask user to input numbers
+# add the entered number to the list
+# if user entered an invalid input, stop the program
+# print the lowest number that has been entered
