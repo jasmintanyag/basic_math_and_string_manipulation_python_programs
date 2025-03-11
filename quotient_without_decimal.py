312f2e77d3759de0a3e8be5f4ff0978ccc732a07
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:22:18 +0100

    Added pseudocode

---
+++ quotient_without_decimal.py
@@ -0,0 +1,7 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+# ask user to input 2 numbers
+# check if the second number is 0
+# if zero, print "Cannot divide by zero."
+# else, perform a floor division
+# convert the result into integer
