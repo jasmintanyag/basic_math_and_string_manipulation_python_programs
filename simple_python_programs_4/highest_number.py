c16fdc3e5bd845d1c8395f85ac37238d5a76d445
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 22:53:25 +0100

    initialize

--- simple_python_programs_4/highest_number.py
+++ simple_python_programs_4/highest_number.py
@@ -1,6 +1,7 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 # initialize variable to store user input, (empty list)
+list_num = []
 # create infinite loop to continuously ask user to input numbers
 # add the entered number to the list
 # if user entered an invalid input, stop the program
