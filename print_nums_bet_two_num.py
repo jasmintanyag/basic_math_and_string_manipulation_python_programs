611ac608dc5ff33211527764bedf3606b9477de2
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:34:31 +0100

    Convert to integer

--- print_nums_bet_two_num.py
+++ print_nums_bet_two_num.py
@@ -1,8 +1,8 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # ask user to input 2 numbers
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
 # convert into integer
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
 # create loop to get the numbers between 2 number
 # print the numbers in between
