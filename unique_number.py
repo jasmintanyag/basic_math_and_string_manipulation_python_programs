0628d452fc66f29fd83747e11ea71f8e52c0be53
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 17:30:20 +0100

    Added pseudocode

---
+++ unique_number.py
@@ -0,0 +1,13 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# initialize the variables for storing the numbers
+    # list to store user input
+    # list to store unique numbers
+    # dictionary to store number counts
+# ask user to input 10 numbers, using for loop
+# convert into integer
+# add the entered number into the list
+# check how many times the number appear
+# check if the number appeared only once
+# if appeared only once, add it to the list of unique numbers
+# print the unique number/s
