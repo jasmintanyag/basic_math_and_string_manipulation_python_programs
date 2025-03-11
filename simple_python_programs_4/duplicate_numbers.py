c1e3667572c93ba667926d53729b107fae2e232d
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 21:52:30 +0100

    Initialize

--- simple_python_programs_4/duplicate_numbers.py
+++ simple_python_programs_4/duplicate_numbers.py
@@ -1,9 +1,9 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # initialize the variables for storing the numbers
-    # list to store user input
-    # list to store duplicate  numbers
-    # dictionary to store number counts
+num_list = []       # list to store user input
+duplicate_nums = [] # list to store duplicate numbers
+num_counts = {}     # dictionary to store number counts
 # ask user to input 10 numbers, using for loop
 # convert into integer
 # add the entered number into the list
