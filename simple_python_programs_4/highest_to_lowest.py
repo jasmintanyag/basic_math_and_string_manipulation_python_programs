99125a50e8b1d7c10422b30236748bdcf42c142a
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 23:15:41 +0100

    add the entered number to the list

--- simple_python_programs_4/highest_to_lowest.py
+++ simple_python_programs_4/highest_to_lowest.py
@@ -1,12 +1,13 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
 # initialize variable to store user input, (empty list)
-list_of_all_nums = []
+list_nums = []
 # create infinite loop to continuously ask user to input numbers
 while  True:
     try:
         num = float(input("Enter a number: "))
 # add the entered number to the list
+        list_nums.append(num)
 # if user entered an invalid input, stop the program
 # sort the entered numbers from highest to  lowest using sort(reverse) function
 # display the numbers from highest to  lowest
