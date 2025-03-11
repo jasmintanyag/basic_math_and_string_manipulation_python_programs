8143f89af550144746bbf17f33af198a8406365f
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:48:35 +0100

    Rearrange

--- remainder_two_num.py
+++ remainder_two_num.py
@@ -1,7 +1,10 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
-# ask user to input 2 numbers
+'''
+ask user to input 2 numbers
+print the remainder of first num and second num
+'''
+
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
-# print the remainder of first num and second num
-print(num1 % num2)
+print(f"The remainder is: {num1 % num2}")
