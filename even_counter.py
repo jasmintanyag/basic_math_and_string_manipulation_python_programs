d129480ba08f1f46e9229827822b9222881fe867
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 13:40:12 +0100

    Convert to float

--- even_counter.py
+++ even_counter.py
@@ -5,8 +5,8 @@ even_count = 0
 # loop from 1 to 10
 for i in range(10):
 # ask the user to enter a number 10 times (since the range is 10)
-    num = input(f"Enter a number ({i+1}): ")
 # convert the input to a float
+    num = float(input(f"Enter a number ({i+1}): "))
 # check if the number is divisible by 2
 # if it is divisible, add 1 to odd count 
 # print the odd count
