5e4243a8f2ddaf19659c01f910ab9182a4d9497c
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 23:31:52 +0100

    Added pseudocode

---
+++ simple_python_programs_4/average_of_nums.py
@@ -0,0 +1,10 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# initialize variable to store user input, (empty list)
+# create infinite loop to continuously ask user to input numbers
+# add the entered number to the list
+# if user entered an invalid input, stop the program
+# check if there's at least 1 num entered
+# calculate average
+# print average
+# if no valid input, print "no valid numbers entered"
