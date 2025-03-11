4c408227605da06852f16692d4a445e807f4447a
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 20:23:42 +0100

    Added pseudocode

---
+++ unique_or_duplicate.py
@@ -0,0 +1,8 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+# initialize variable to store user input
+# create infinite loop to continuously ask user to input numbers
+# check if the user already entered the same number
+    # if yes, print "Duplicate"
+    # else, print "Unique"
+# if the user input is invalid, stop the program
