6ccbcda702a54909c10092a63d525336fe4c4995
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 20:26:26 +0100

    Created infinite loop, asks user to input numbers

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -3,6 +3,9 @@
 # initialize variable to store user input
 list_nums = []
 # create infinite loop to continuously ask user to input numbers
+while True:
+    try:
+        num = float(input("Enter a number: "))
 # check if the user already entered the same number
     # if yes, print "Duplicate"
     # else, print "Unique"
