e4803797c2a44a113ff8d36c2f77e83fc896998c
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:43:01 +0100

    Fixed the error

--- print_nums_bet_two_num.py
+++ print_nums_bet_two_num.py
@@ -4,9 +4,13 @@
 # convert into integer
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
+
+# The numbers between does not print if num1 is larger than num2
+# Trying the swapping method
+if num1 > num2:
+    num1, num2 = num2, num1
+
 # create loop to get the numbers between 1st num and 2nd num
 for numbers in range(num1 + 1, num2):
 # print the numbers in between
     print(numbers)
-
-# The numbers between does not print if num1 is larger than num2
