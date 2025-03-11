0693d2782fed04b53c8f6a537a21fa40fe24816e
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 03:04:24 +0100

    Ak user to input 2 numbers

--- diff_of_two_numbers.py
+++ diff_of_two_numbers.py
@@ -1,4 +1,6 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # ask user to input 2 numbers
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
 # print the difference of two numbers
