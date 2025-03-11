9b45a4ee1fe867fa80813966e5d11f43a0d4409f
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 23:13:28 +0100

    Initialize

--- simple_python_programs_4/highest_to_lowest.py
+++ simple_python_programs_4/highest_to_lowest.py
@@ -1,6 +1,7 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
 # initialize variable to store user input, (empty list)
+list_of_all_nums = []
 # create infinite loop to continuously ask user to input numbers
 # add the entered number to the list
 # if user entered an invalid input, stop the program
