7fe6c954657f94deef40cf70e7f19d0a224080ec
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-11 14:49:10 +0100

    Rearrange

--- print_nums_bet_two_num.py
+++ print_nums_bet_two_num.py
@@ -1,16 +1,16 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
-# ask user to input 2 numbers
-# convert into integer
+'''
+ask user to input 2 numbers
+convert into integer
+if num1 is larger than num2, swap their values
+create loop to get the numbers between 1st num and 2nd num
+print the numbers in between
+'''
+
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
-
-# The numbers between does not print if num1 is larger than num2
-# Trying the swapping method
 if num1 > num2:
     num1, num2 = num2, num1
-
-# create loop to get the numbers between 1st num and 2nd num
 for numbers in range(num1 + 1, num2):
-# print the numbers in between
     print(numbers, end= " ")
