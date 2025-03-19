b1731e1a2aba7523c65ed8cc47764b81a402cf98
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:26:46 +0800

    ask user input

--- string_manipulation/pascal_case.py
+++ string_manipulation/pascal_case.py
@@ -1,4 +1,5 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 # ask user input their name in incorrect casing
+name = input("Enter your full name in incorrect casing: ")
 # print input in pascal case
