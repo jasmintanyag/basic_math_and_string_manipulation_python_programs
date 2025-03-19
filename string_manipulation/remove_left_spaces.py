dd771f7080aadedb23be9bf2a9372b84c04ae6c7
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:27:34 +0800

    Removed the spaces

--- string_manipulation/remove_left_spaces.py
+++ string_manipulation/remove_left_spaces.py
@@ -1,6 +1,6 @@
 # Prog01: Create a program that ask the user to input their full name with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # ask user to input their full name with several spaces at the beginning
-name = input("Enter your full name (add space/s at the beginning): ")
 # remove the spaces in the beginning using the lft strip method
+name = input("Enter your full name (add space/s at the beginning): ").lstrip()
 # print the input without spaces at beginning
