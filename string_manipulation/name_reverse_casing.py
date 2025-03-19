80acc1550a800a5f37a9286bafd2087d2db3edc6
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:06:51 +0800

    Rearrange and added display texts

--- string_manipulation/name_reverse_casing.py
+++ string_manipulation/name_reverse_casing.py
@@ -1,6 +1,7 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 # ask user to input their full name in incorrect casing
-name = input("Enter your full name in incorrect casing: ")
 # print each character input in reverse casing
-print(name.swapcase())
+
+name = input("Enter your full name in incorrect casing: ")
+print("Reversed case: ", name.swapcase())
