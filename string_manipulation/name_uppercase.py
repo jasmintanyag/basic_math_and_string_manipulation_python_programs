10341a49c556b682eba2fe22e68b12a6d8aaf2a0
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:49:06 +0800

    Rearrange and added display texts

--- string_manipulation/name_uppercase.py
+++ string_manipulation/name_uppercase.py
@@ -1,6 +1,7 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # ask user to input their full name
-name = input("Enter your full name: ")
 # print the input in all capital letters
-print(name.upper())
+
+name = input("Enter your full name: ")
+print("Your name is: ", name.upper())
