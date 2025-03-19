4bf2c26c6ba8d26bb5725d99d817b610b8750839
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:17:49 +0800

    Added pseudocode

---
+++ string_manipulation/num_of_characters.py
@@ -0,0 +1,4 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+# ask user input their full name
+# print number of characters in input
