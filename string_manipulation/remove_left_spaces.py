24fc371f5bba7ee704ed8f969539615f4276cd36
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:23:47 +0800

    Added pseudocode

---
+++ string_manipulation/remove_left_spaces.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask the user to input their full name with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# ask user to input their full name with several spaces at the beginning
+# remove the spaces in the beginning using the lft strip method
+# print the input without spaces at beginning
