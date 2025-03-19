aab09bbb970746f3ead177ca0f8ea67380da2e4e
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:29:49 +0800

    Rearrange and added display texts

--- string_manipulation/pascal_case.py
+++ string_manipulation/pascal_case.py
@@ -1,6 +1,7 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 # ask user input their name in incorrect casing
-name = input("Enter your full name in incorrect casing: ")
 # print input in pascal case
-print(name.title().replace(" ", ""))
+
+name = input("Enter your full name in incorrect casing: ")
+print("Name in Pascal Case: ", name.title().replace(" ", ""))
