1ff2afb364715a5904851b2a7a38cd124c314bdc
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:42:57 +0800

    Added pseudocode

---
+++ string_manipulation/name_uppercase.py
@@ -0,0 +1,4 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# ask user to input their full name
+# print the input in all capital letters
