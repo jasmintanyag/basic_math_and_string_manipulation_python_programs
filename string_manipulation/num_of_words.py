119684c3d19538ca5a1ac968ba64ae299ab45fe2
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:12:25 +0800

    ask user input a complete statement

--- string_manipulation/num_of_words.py
+++ string_manipulation/num_of_words.py
@@ -1,4 +1,5 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 # ask user input a complete statement
+statement = input("Enter a complete statement: ")
 # print the number of words in the input
