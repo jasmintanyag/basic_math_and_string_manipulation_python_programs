b01f1f2fbbc67e2979f5c2a0b47b28cddcffacda
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:38:41 +0800

    fixed typo

--- string_manipulation/remove_left_spaces.py
+++ string_manipulation/remove_left_spaces.py
@@ -1,7 +1,7 @@
 # Prog01: Create a program that ask the user to input their full name with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # ask user to input their full name with several spaces at the beginning
-# remove the spaces in the beginning using the lft strip method
+# remove the spaces in the beginning using the left strip method
 # print the input without spaces at beginning
 
 name = input("Enter your full name (add space/s at the beginning): ").lstrip()
