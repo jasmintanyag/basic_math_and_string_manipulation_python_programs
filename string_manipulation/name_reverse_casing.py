4a7ced31d215664360fb6a1da65eb4dbaa07f544
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:03:09 +0800

    Added pseudocode

---
+++ string_manipulation/name_reverse_casing.py
@@ -0,0 +1,4 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# ask user to input their full name in incorrect casing
+# print each character input in reverse casing
