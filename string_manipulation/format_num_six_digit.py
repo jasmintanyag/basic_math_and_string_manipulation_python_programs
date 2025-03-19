09f2406bc180b85da6dd87880c8d2fa942b246e3
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:35:32 +0800

    Added pseudocode

---
+++ string_manipulation/format_num_six_digit.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+# ask user to input a number from 0-1000
+# convert input to integer
+# print the number in a 6 digit format, adding zero's at beginning
