940e6923668a5ae74d38b19cf0490e2cd095b157
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:22:26 +0800

    Rearrange and added display texts

--- string_manipulation/num_of_characters.py
+++ string_manipulation/num_of_characters.py
@@ -1,6 +1,7 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # ask user input their full name
-name = input("Enter your full name: ")
 # print number of characters in input
-print(len(name.replace(" ", "")))
+
+name = input("Enter your full name: ")
+print("Number of characters: ", len(name.replace(" ", "")))
