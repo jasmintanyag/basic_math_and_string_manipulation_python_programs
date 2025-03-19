283545eb267fc1c1607948760ee4205a64a9ba39
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:34:41 +0800

    Rearrange and added display texts

--- string_manipulation/snake_case.py
+++ string_manipulation/snake_case.py
@@ -1,6 +1,7 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 # ask user input their name in incorrect casing
-name = input("Enter your full name in incorrect casing: ")
 # print input in snake case
-print(name.lower().replace(" ", "_"))
+
+name = input("Enter your full name in incorrect casing: ")
+print("Name in Snake Case: ", name.lower().replace(" ", "_"))
