d5b69abc58dccd9fdb6f98dfa059af81c37f5831
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:31:14 +0800

    Added pseudocode

---
+++ string_manipulation/snake_case.py
@@ -0,0 +1,4 @@
+# Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# ask user input their name in incorrect casing
+# print input in snake case
