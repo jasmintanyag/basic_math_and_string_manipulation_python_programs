7f0c5389e6e8d842e5f80dd01e11a6c92eb94eb1
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:30:16 +0800

    Rearrange and added display texts

--- string_manipulation/remove_left_spaces.py
+++ string_manipulation/remove_left_spaces.py
@@ -2,6 +2,7 @@
 
 # ask user to input their full name with several spaces at the beginning
 # remove the spaces in the beginning using the lft strip method
-name = input("Enter your full name (add space/s at the beginning): ").lstrip()
 # print the input without spaces at beginning
-print(name)
+
+name = input("Enter your full name (add space/s at the beginning): ").lstrip()
+print("Your name is: ", name)
