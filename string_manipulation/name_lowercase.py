4923b44e5357fca4a4c8d9ef9eeba81fda844964
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:52:07 +0800

    Ask user input their full name

--- string_manipulation/name_lowercase.py
+++ string_manipulation/name_lowercase.py
@@ -1,4 +1,5 @@
 # Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 # ask user to input their full name
+name = input("Enter your full name: ")
 # print the input in all lower case letters
