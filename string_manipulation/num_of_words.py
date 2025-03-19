5a0a431debd05c9c369ffc628eb6fe2d7b7b5eab
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:11:28 +0800

    Added pseudocode

---
+++ string_manipulation/num_of_words.py
@@ -0,0 +1,4 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# ask user input a complete statement
+# print the number of words in the input
