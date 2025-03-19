957a38ebd0c1381a269a4f5a679df8549842b2a9
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:15:24 +0800

    Rearrange and added disply texts

--- string_manipulation/num_of_words.py
+++ string_manipulation/num_of_words.py
@@ -1,6 +1,7 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 # ask user input a complete statement
-statement = input("Enter a complete statement: ")
 # print the number of words in the input
-print(len(statement.split()))
+
+statement = input("Enter a complete statement: ")
+print("Number of words: ", len(statement.split()))
