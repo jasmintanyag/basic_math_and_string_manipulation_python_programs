ef82943f20d67e41560b5e7c3f6c94ffbf3de70b
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:44:07 +0800

    Ask user to input their full name

--- string_manipulation/name_uppercase.py
+++ string_manipulation/name_uppercase.py
@@ -1,4 +1,5 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # ask user to input their full name
+name = input("Enter your full name: ")
 # print the input in all capital letters
