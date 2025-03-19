37f1c285cfea515f22afa768d3c466e052b28b97
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 22:51:11 +0800

    Added pseudocode

---
+++ string_manipulation/name_lowercase.py
@@ -0,0 +1,4 @@
+# Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+# ask user to input their full name
+# print the input in all lower case letters
