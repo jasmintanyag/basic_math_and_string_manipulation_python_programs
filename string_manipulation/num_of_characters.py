207c9b80d75e69f42cbc29b4c6fedbca88c3e802
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-19 23:18:35 +0800

    Ask user input

--- string_manipulation/num_of_characters.py
+++ string_manipulation/num_of_characters.py
@@ -1,4 +1,5 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # ask user input their full name
+name = input("Enter your full name: ")
 # print number of characters in input
