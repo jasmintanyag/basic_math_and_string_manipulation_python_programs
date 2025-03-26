663694d556151475564ceb9e8b043d1b1e55870e
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 02:26:30 +0800

    rearranging

--- string_manipulation_2/like_lstrip.py
+++ string_manipulation_2/like_lstrip.py
@@ -1,13 +1,14 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # ask user to input a string
-text =  input("Enter a string: ")
 # initialize a counter
-i = 0
 # create while-loop
 # if i is less than the length of s, and the character at i is space, 
-while i < len(text) and text[i] == ' ':
 # move to the next character until a non-space character is found
-    i += 1
 # print starting from index i
+
+text =  input("Enter a string: ")
+i = 0
+while i < len(text) and text[i] == ' ':
+    i += 1
 print("Result: ", text[1:])
