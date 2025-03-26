1a0874081fad7a468a67e116ce288019758caaab
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 02:04:20 +0800

    Added pseudocode

---
+++ string_manipulation_2/like_lstrip.py
@@ -0,0 +1,8 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# ask user to input a string
+# initialize a counter
+# create while-loop
+# if i is less than the length of s, and the character at i is space, 
+# move to the next character until a non-space character is found
+# print starting from index i
