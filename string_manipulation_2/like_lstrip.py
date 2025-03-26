546e2adc5f75a54c31d4b5a3dd96746b9df3738d
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 02:25:29 +0800

    tried changing the variable name

--- string_manipulation_2/like_lstrip.py
+++ string_manipulation_2/like_lstrip.py
@@ -1,13 +1,13 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # ask user to input a string
-user_inp =  input("Enter a string: ")
+text =  input("Enter a string: ")
 # initialize a counter
 i = 0
 # create while-loop
 # if i is less than the length of s, and the character at i is space, 
-while i < len(user_inp) and user_inp[i] == " ":
+while i < len(text) and text[i] == ' ':
 # move to the next character until a non-space character is found
     i += 1
 # print starting from index i
-print("Result: ", user_inp[1:])
+print("Result: ", text[1:])
