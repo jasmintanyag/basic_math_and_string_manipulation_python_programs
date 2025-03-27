1c0bfe75675c323cae01da6e7304f5e80ac8e32b
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 14:07:11 +0800

    assume that characters are all uppercase

--- string_manipulation_2/conver_to_uppercase.py
+++ string_manipulation_2/conver_to_uppercase.py
@@ -1,7 +1,9 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # ask user to input a text/string
+text = input("Enter a text: ")
 # assume that the text is all uppercase letters initially
+all_uppercase = True
 # create for-loop in every character in text/string
 # check if char is in lowercase
 # if at least one lowercase letter is found, set it to false then exit the loop
