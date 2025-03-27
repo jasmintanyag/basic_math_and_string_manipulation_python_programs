bb432e4276ce483dae70915b47099ed8a71f1bdc
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:53:37 +0800

    rearrange and added display texts

--- string_manipulation_2/capital_case.py
+++ string_manipulation_2/capital_case.py
@@ -1,19 +1,20 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # ask user to input a text/string
-text = input("Enter a text: ")
 # in the string entered; if first character is lowercase, convert it to uppercase
+# for the rest of the characters; if uppercase, convert it to lowercase
+# else, remain unchange
+# print the result
+
+text = input("Enter a text: ")
 if text:
     first_character = text[0]
     if 'a' <= first_character <= 'z':
         first_character = chr(ord(first_character) - 32)
     capital_case = first_character
-# for the rest of the characters; if uppercase, convert it to lowercase
     for char in text[1:]:
         if 'A' <= char <= 'Z':
             capital_case += chr(ord(char) + 32)
-# else, remain unchange
         else:
             capital_case += char
-# print the result
-print(capital_case)
+print("Capitalized result: ", capital_case)
