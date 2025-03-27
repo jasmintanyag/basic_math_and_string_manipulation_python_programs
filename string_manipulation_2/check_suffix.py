bf437e5e487fcb1608c74965bb9b5cd7816c2cc5
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 14:36:12 +0800

    rearrange and added display texts

--- string_manipulation_2/check_suffix.py
+++ string_manipulation_2/check_suffix.py
@@ -1,15 +1,16 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # ask user to input text/string
-text = input("Enter a text: ")
 # ask again to input the suffix they want to check
-suffix = input("Enter suffix to check: ")
 # checks if the extracted suffix is equal to the inputted suffix
-if text[-len(suffix): ] == suffix:
 # if yes, the condition is true
-    matched_to_suffix = True
 # else, it is false
+# print the result
+
+text = input("Enter a text: ")
+suffix = input("Enter suffix to check: ")
+if text[-len(suffix): ] == suffix:
+    matched_to_suffix = True
 else:
     matched_to_suffix = False
-# print the result
-print(matched_to_suffix)
+print("Suffix matched: ", matched_to_suffix)
