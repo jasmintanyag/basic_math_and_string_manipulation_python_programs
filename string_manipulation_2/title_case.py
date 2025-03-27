830333d7ea2b2b72f81c13c3af5e3b8a80faa8f7
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 16:02:20 +0800

    ask user input

--- string_manipulation_2/title_case.py
+++ string_manipulation_2/title_case.py
@@ -1,6 +1,7 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # ask user to input a text/string
+text = input("Enter a text: ")
 # split the string in words
 # convert every first character of words to uppercase, while the rest of characters remain lowercase
 # join the words into a string
