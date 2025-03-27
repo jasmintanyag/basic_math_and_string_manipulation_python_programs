c282c15d65e57d54e980059f4d6f90bed61709f0
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 13:41:27 +0800

    added pseudocode

---
+++ string_manipulation_2/convert_to_lowercase.py
@@ -0,0 +1,9 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# ask user to input a text/string
+# initialize empty string to store the converted result
+# create for-loop in every character in text/string
+# checks if char is an uppercase letter using ASCII value comparison
+# if uppercase, convert to lowercase by adding 32 to its ASCII value
+# else, if already in lowercase, remain unchanged
+# print the lowercase version
