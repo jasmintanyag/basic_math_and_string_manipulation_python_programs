4ca96a63100d478085c0bd747208b36fb8ddd7b9
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 13:43:22 +0800

    ask user input and initialize

--- string_manipulation_2/convert_to_lowercase.py
+++ string_manipulation_2/convert_to_lowercase.py
@@ -1,7 +1,9 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # ask user to input a text/string
+text = input("Enter a text: ")
 # initialize empty string to store the converted result
+lowercase_result = ""
 # create for-loop in every character in text/string
 # checks if char is an uppercase letter using ASCII value comparison
 # if uppercase, convert to lowercase by adding 32 to its ASCII value
