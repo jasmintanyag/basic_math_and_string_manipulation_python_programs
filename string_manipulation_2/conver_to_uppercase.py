b9aadbaf3b70cd0212841a5c914771cb58af3845
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 14:12:33 +0800

    for-loop to check if char is in lowercase

--- string_manipulation_2/conver_to_uppercase.py
+++ string_manipulation_2/conver_to_uppercase.py
@@ -5,6 +5,8 @@ text = input("Enter a text: ")
 # assume that the text is all uppercase letters initially
 all_uppercase = True
 # create for-loop in every character in text/string
+for char in text:
 # check if char is in lowercase
+    if 'a' <= char <= 'z':
 # if at least one lowercase letter is found, set it to false then exit the loop
 # print True if all characters are uppercase; false if not
