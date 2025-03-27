4e1c3e8628d8084b06ef763405ebeb05d9f6a566
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:46:08 +0800

    if first character is lowercase, convert it to uppercase

--- string_manipulation_2/capital_case.py
+++ string_manipulation_2/capital_case.py
@@ -3,6 +3,10 @@
 # ask user to input a text/string
 text = input("Enter a text: ")
 # in the string entered; if first character is lowercase, convert it to uppercase
+if text:
+    first_character = text[0]
+    if 'a' <= first_character <= 'z':
+        first_character = chr(ord(first_character) - 32)
 # for the rest of the characters; if uppercase, convert it to lowercase
 # else, remain unchange
 # print the result
