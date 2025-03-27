426d4b60c0cfd06be998af9f4aea68b82279caab
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:34:25 +0800

    rearrange and added display texts

--- string_manipulation_2/reverse_case.py
+++ string_manipulation_2/reverse_case.py
@@ -1,19 +1,20 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # ask user to input a text/string
-text = input("Enter a text: ")
 # initialize empty string to store the converted result
-reverse_case = ""
 # create for-loop in every character in text/string
-for char in text:
 # checks if char is an uppercase, then convert it to lowercase
+# checks if char is an lowercase, then convert it to uppercase
+# else, remain unchange
+# print the swapcase version
+
+text = input("Enter a text: ")
+reverse_case = ""
+for char in text:
     if 'a' <= char <= 'z':
         reverse_case += chr(ord(char) - 32)
-# checks if char is an lowercase, then convert it to uppercase
     elif 'A' <= char <= 'Z':
         reverse_case += chr(ord(char) + 32)
-# else, remain unchange
     else:
         reverse_case += char
-# print the swapcase version
-print(reverse_case)
+print("Swapcase: ", reverse_case)
