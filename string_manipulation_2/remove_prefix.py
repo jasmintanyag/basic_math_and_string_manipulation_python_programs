1d6bfc1197e3c4be37329f10f39a235fee4bb1d2
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 12:31:23 +0800

    added pseudocode

---
+++ string_manipulation_2/remove_prefix.py
@@ -0,0 +1,7 @@
+# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+# ask user to input a string
+# ask again to input the prefix they want to remove
+# check if the text/string they inputted starts with the prefix
+# if yes, remove it by slicing from the length of prefix onwards
+# if not, print the original text 
