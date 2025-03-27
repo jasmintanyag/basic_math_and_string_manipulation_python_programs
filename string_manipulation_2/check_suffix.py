0beac57b1e312f89b1cb74e252f9e02d3a221e29
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 14:29:02 +0800

    added pseudocode

---
+++ string_manipulation_2/check_suffix.py
@@ -0,0 +1,8 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# ask user to input text/string
+# ask again to input the suffix they want to check
+# checks if the extracted suffix is equal to the inputted suffix
+# if yes, the condition is true
+# else, it is false
+# print the result
