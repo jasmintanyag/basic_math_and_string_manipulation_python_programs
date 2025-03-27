00dbacb07c1f77507f0ba453cb26d1270a261b62
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:05:00 +0800

    added pseudocode

---
+++ string_manipulation_2/center_string.py
@@ -0,0 +1,7 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# ask user input text/string
+# ask again for their targeted length
+# calculate the total spaces needed to center the text
+# divide spaces equally to left and right
+# print the string in the center
