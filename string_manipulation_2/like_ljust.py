ce6fa595c603d9e69063aab263c49df83957766e
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 14:56:23 +0800

    rearrange and added display texts

--- string_manipulation_2/like_ljust.py
+++ string_manipulation_2/like_ljust.py
@@ -1,11 +1,12 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # ask user input text/string
-text = input("Enter a text: ")
 # ask again for their targeted length
-target_length = int(input("What is your targeted length? "))
 # if the string entered is shorter than their targeted length, add spaces to the right side
+# print the modified string / result
+
+text = input("Enter a text: ")
+target_length = int(input("What is your targeted length? "))
 if len(text) < target_length:
     text = text + " " * (target_length - len(text))
-# print the modified string / result
-print(text)
+print(f"Result: '{text}'")
