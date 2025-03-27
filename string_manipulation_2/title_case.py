f5222d55c741bb566914e04b1e1b9adcc274f56c
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 16:12:43 +0800

    rearranged and added display texts

--- string_manipulation_2/title_case.py
+++ string_manipulation_2/title_case.py
@@ -1,12 +1,13 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # ask user to input a text/string
-text = input("Enter a text: ")
 # split the string in words
-words_from_text = text.split()
 # convert every first character of words to uppercase, while the rest of characters remain lowercase
-title_cased = [word[:1].upper() + word[1:].lower() for word in words_from_text]
 # join the words into a string
-title_cased = " ".join(title_cased)
 # print the result
-print(title_cased)
+
+text = input("Enter a text: ")
+words_from_text = text.split()
+title_cased = [word[:1].upper() + word[1:].lower() for word in words_from_text]
+title_cased = " ".join(title_cased)
+print("Title cased: ", title_cased)
