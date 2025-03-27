5dea08dffe872894d7f7803aee2a46322bcf05b7
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:43:55 +0800

    ask user input

--- string_manipulation_2/capital_case.py
+++ string_manipulation_2/capital_case.py
@@ -1,6 +1,7 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # ask user to input a text/string
+text = input("Enter a text: ")
 # in the string entered; if first character is lowercase, convert it to uppercase
 # for the rest of the characters; if uppercase, convert it to lowercase
 # else, remain unchange
