895f02347c689b166eb385a310e7f9dbd04508cd
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:16:32 +0800

    rearrange and added display texts

--- string_manipulation_2/center_string.py
+++ string_manipulation_2/center_string.py
@@ -1,17 +1,18 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # ask user input text/string
-text = input("Enter a text: ")
 # ask again for their targeted length
-target_length = int(input("What is your targeted length? "))
 # if the string entered is shorter than their targeted length, add spaces to the right side
-if len(text) < target_length:
 # calculate the total spaces needed to center the text
-    total_space = target_length - len(text)
 # divide spaces equally to left and right
+# add spaces to left and right side
+# print the string in the center
+
+text = input("Enter a text: ")
+target_length = int(input("What is your targeted length? "))
+if len(text) < target_length:
+    total_space = target_length - len(text)
     left_space = total_space // 2
     right_space = total_space - left_space
-# add spaces to left and right side
     text = " " * left_space + text + " " * right_space
-# print the string in the center
-print(text)
+print(f"Result: '{text}'")
