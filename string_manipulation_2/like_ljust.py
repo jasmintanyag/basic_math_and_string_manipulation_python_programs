0f3dd97d547616e37d13384a2ede5507c5e8ce74
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 14:48:48 +0800

    added pseudocode

---
+++ string_manipulation_2/like_ljust.py
@@ -0,0 +1,6 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# ask user input text//string
+# ask again for their targeted length
+# if the string entered is shorter than their targeted length, add spaces to the right side
+# print the modified string / result
