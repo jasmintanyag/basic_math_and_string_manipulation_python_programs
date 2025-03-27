ecd513ddec3556d845183ced2b984299fc4b0de0
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 13:45:14 +0800

    for-loop, checks if char is uppercase

--- string_manipulation_2/convert_to_lowercase.py
+++ string_manipulation_2/convert_to_lowercase.py
@@ -5,7 +5,9 @@ text = input("Enter a text: ")
 # initialize empty string to store the converted result
 lowercase_result = ""
 # create for-loop in every character in text/string
+for char in text:
 # checks if char is an uppercase letter using ASCII value comparison
+    if 'A' <= char <= 'Z':
 # if uppercase, convert to lowercase by adding 32 to its ASCII value
 # else, if already in lowercase, remain unchanged
 # print the lowercase version
