06a1f7ab97612451600c3e11086687ad78acd398
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-27 15:28:14 +0800

    if lower, convert to upper

--- string_manipulation_2/reverse_case.py
+++ string_manipulation_2/reverse_case.py
@@ -5,7 +5,10 @@ text = input("Enter a text: ")
 # initialize empty string to store the converted result
 reverse_case = ""
 # create for-loop in every character in text/string
+for char in text:
 # checks if char is an uppercase, then convert it to lowercase
+    if 'a' <= char <= 'z':
+        reverse_case += chr(ord(char) - 32)
 # checks if char is an lowercase, then convert it to uppercase
 # else, remain unchange
 # print the swapcase version
