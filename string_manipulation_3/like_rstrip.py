9b0628209000c50735b4d96320c30bb531752f19
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 00:59:46 +0800

    ask user input

--- string_manipulation_3/like_rstrip.py
+++ string_manipulation_3/like_rstrip.py
@@ -1,6 +1,7 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # ask user to input a string
+text = input("Enter a string: ")  
 # initialize a counter from the end of the string
 # create while-loop to find the last non-space character
 # print the string without the spaces
