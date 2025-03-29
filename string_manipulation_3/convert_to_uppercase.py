68ae9e46d151f16c349ebc31f33ce3a443c8a5e9
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 01:30:22 +0800

    check if lowercase, if true, convert it to uppercase

--- string_manipulation_3/convert_to_uppercase.py
+++ string_manipulation_3/convert_to_uppercase.py
@@ -5,7 +5,10 @@ text = input("Enter a text: ")
 # initialize empty string to store the converted result
 uppercase_result = ""
 # create for-loop in every character in text/string
+for char in text:
 # checks if char is an lowercase letter using ASCII value comparison
+    if 'a' <= char <= 'z':
 # if lowercase, convert to uppercase by subtracting 32 to its ASCII value
+        uppercase_result += chr(ord(char) - 32)
 # else, if already in uppercase, remain unchanged
 # print the uppercase version
