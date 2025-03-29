a8cf8c9c1e0826e0468da61f3d17e00383e6fc67
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 02:11:52 +0800

    rearrange and added display texts

--- string_manipulation_3/check_prefix.py
+++ string_manipulation_3/check_prefix.py
@@ -1,15 +1,16 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # ask user to input text/string
-text = input("Enter a text: ")
 # ask again to input the prefix they want to check
-prefix = input("Enter prefix to check: ")
 # checks if the extracted prefix is equal to the inputted prefix
-if text[:len(prefix)] == prefix:
 # if yes, the condition is true
-    matched_to_prefix = True
 # else, it is false
+# print the result
+
+text = input("Enter a text: ")
+prefix = input("Enter prefix to check: ")
+if text[:len(prefix)] == prefix:
+    matched_to_prefix = True
 else:
     matched_to_prefix = False
-# print the result
 print("Prefix matched: ", matched_to_prefix)
