0c82bbd0af402bd13d7f9fc40396a1c690697c34
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 01:06:21 +0800

    rearrange and added display texts

--- string_manipulation_3/like_rstrip.py
+++ string_manipulation_3/like_rstrip.py
@@ -1,11 +1,12 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # ask user to input a string
-text = input("Enter a string: ")  
 # initialize a counter from the end of the string
-i = len(text) - 1  
 # create while-loop to find the last non-space character
+# print the string without the spaces
+
+text = input("Enter a string: ")  
+i = len(text) - 1  
 while i >= 0 and text[i] == ' ':
     i -= 1
-# print the string without the spaces
-print(text[:i+1])
+print("Result: ", text[:i+1])
