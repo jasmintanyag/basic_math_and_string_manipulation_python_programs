18d89daca91c7f3fb2b13104943e66cc1d8299d3
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 02:00:10 +0800

    added pseudocode

---
+++ string_manipulation_3/check_prefix.py
@@ -0,0 +1,8 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# ask user to input text/string
+# ask again to input the prefix they want to check
+# checks if the extracted prefix is equal to the inputted prefix
+# if yes, the condition is true
+# else, it is false
+# print the result
