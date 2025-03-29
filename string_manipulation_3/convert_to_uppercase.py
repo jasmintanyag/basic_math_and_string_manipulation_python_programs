60c37c21e316609bb2fe61d3edcf3496d1acb1af
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 01:26:15 +0800

    ask user input

--- string_manipulation_3/convert_to_uppercase.py
+++ string_manipulation_3/convert_to_uppercase.py
@@ -1,6 +1,7 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # ask user to input a text/string
+text = input("Enter a text: ")
 # initialize empty string to store the converted result
 # create for-loop in every character in text/string
 # checks if char is an lowercase letter using ASCII value comparison
