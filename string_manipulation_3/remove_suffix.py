95323191a549fd0227e98ac9c3897845a4bbefcf
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 01:19:04 +0800

    check if ends with suffix, if yes, print the result

--- string_manipulation_3/remove_suffix.py
+++ string_manipulation_3/remove_suffix.py
@@ -5,5 +5,7 @@ text = input("Enter a string: ")
 # ask again to input the suffix they want to remove
 suffix = input("Enter the suffix you want to remove: ")
 # check if the text/string they inputted ends with the suffix
+if text.endswith(suffix):
 # if yes, remove it by slicing from the length of suffix
+    print("Removed Prefix Result: ", text[:-len(suffix):])
 # if not, print the original text 
