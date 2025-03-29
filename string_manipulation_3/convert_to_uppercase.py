d0841303d8d2d35ae00689574c21e77c63751a9e
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 01:25:18 +0800

    added pseudocode

---
+++ string_manipulation_3/convert_to_uppercase.py
@@ -0,0 +1,9 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# ask user to input a text/string
+# initialize empty string to store the converted result
+# create for-loop in every character in text/string
+# checks if char is an lowercase letter using ASCII value comparison
+# if lowercase, convert to uppercase by subtracting 32 to its ASCII value
+# else, if already in uppercase, remain unchanged
+# print the uppercase version
