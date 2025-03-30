0f0c3bf93bf9b42cb9f7ef05f2e3cfb288e28212
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 20:21:43 +0800

    added pseudocode

---
+++ like_count.py
@@ -0,0 +1,8 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# ask user to input a string
+# ask again to input a substring to count
+# initialize a counter
+# loop through string and count its occurences
+# check if substring matches, add 1 to count if matched
+# print the result
