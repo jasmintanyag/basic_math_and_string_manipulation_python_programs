64d8d76a4f95eca4de3cda41c506056f74d43e06
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-31 05:19:39 +0800

    added pseudocode

---
+++ string_manipulation_3/like_rindex.py
@@ -0,0 +1,10 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# ask user to input a string
+# ask again to input the substring to find
+# initialize the index variable
+# loop through the string from left to right
+# check if substring matches the current position
+# store the index and stop the loop
+# if index is still -1, it means the substring was not found
+# else, print the result if found
