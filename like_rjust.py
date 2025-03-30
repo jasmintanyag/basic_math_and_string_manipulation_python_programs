95f9a2894171ed5f91548eca80a7ff85ea9a9dba
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 14:37:20 +0800

    added pseudocode

---
+++ like_rjust.py
@@ -0,0 +1,6 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# ask user input text/string
+# ask again for their targeted length
+# if the string entered is shorter than their targeted length, add spaces to the left side
+# print the modified string / result
