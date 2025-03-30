299e22722cd792c9b840915d40172831cfd085db
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-31 04:59:25 +0800

    added pseudocode

---
+++ string_manipulation_3/like_index.py
@@ -0,0 +1,10 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# ask user to input a string
+# ask again to input the substring to find
+# initialize the index variable
+# loop through the string to find the first occurrence
+# check if substring matches the current position
+# store the index and stop the loop
+# if index is still -1, it means the substring was not found
+# else, print the result if found
