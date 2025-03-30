24a22a29225200ca47216d9b6170c7997afd51d4
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 19:48:15 +0800

    ask user input

--- like_zfill.py
+++ like_zfill.py
@@ -1,6 +1,8 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # ask user input numbers/string with spaces in beginning
+text = input("Enter a number w/ spaces at beginning: ")
 # ask again for their targeted length
+target_length = int(input("What is your targeted length? "))
 # if the string entered is shorter than their targeted length, change the spaces into zeroes
 # print the modified string / result
