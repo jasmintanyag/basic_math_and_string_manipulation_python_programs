145c2ad63e48b136e7c786b9dea0c59ff9419eb8
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 14:38:24 +0800

    ask user input

--- like_rjust.py
+++ like_rjust.py
@@ -1,6 +1,8 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # ask user input text/string
+text = input("Enter a text: ")
 # ask again for their targeted length
+target_length = int(input("What is your targeted length? "))
 # if the string entered is shorter than their targeted length, add spaces to the left side
 # print the modified string / result
