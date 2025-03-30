618c30e23b88d95643fcf0e3c10b3295a6ee0345
jasmintanyag <tanyagjasmin@gmail.com>
2025-03-30 19:46:39 +0800

    change pseudocode

--- like_zfill.py
+++ like_zfill.py
@@ -1,6 +1,6 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
-# ask user input text/string
+# ask user input numbers/string with spaces in beginning
 # ask again for their targeted length
 # if the string entered is shorter than their targeted length, change the spaces into zeroes
 # print the modified string / result
